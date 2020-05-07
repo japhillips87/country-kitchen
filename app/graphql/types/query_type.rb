e7269979551e44bcb83d2999225f9f0602b51ee6
Josh <japhillps87@gmail.com>
2020-05-07 13:48:53 -0400

    add query to get recipe by name

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,10 +1,18 @@
 module Types
   class QueryType < Types::BaseObject
     field :recipes, [Types::RecipeType], null: false
+    field :recipe, Types::RecipeType, null: false do
+      argument :name, String, required: true
+    end
+
     field :categories, [Types::CategoryType], null: false
 
     def recipes
       Recipe.all
+    end
+
+    def recipe(name:)
+      Recipe.find_by(name: name)
     end
 
     def categories
